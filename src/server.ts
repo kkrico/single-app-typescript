3c4065b4d0426ef555957b5324b11e52582dd9e4
Daniel Ramos <daniel.framos@cassi.com.br>
2020-02-27 15:23:46 -0300

    Melhorado suporte a tipos

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,18 @@ import path = require('path');
 import { router } from './middlewares/routesMiddleware';
 import { Express as ExpressApp } from 'express-serve-static-core';
 
-const apiVersion = '/api/v1';
-const wildCard = '*';
+enum AppConstants {
+    apiVersion = '/api/v1',
+    wildCard = '*',
+}
 
 export default (): ExpressApp => {
     const app = express();
 
-    app.use(apiVersion, router);
+    app.use(AppConstants.apiVersion, router);
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get(wildCard, (_req, res) => {
+    app.get(AppConstants.apiVersion, (_req, res) => {
         res.sendFile(path.join(__dirname + '/client/dist/index.html'));
     });
 
